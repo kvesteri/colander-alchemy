edf32e004d0c4b721e7c8ab01dd0b8230931ee5e
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-07-23 09:48:00 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+colander-alchemy
+-------------------
+
+Generates colander schemas from SQLAlchemy models.
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='colander-alchemy',
+    version='0.1.0',
+    url='https://github.com/kvesteri/colander-alchemy',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description='Generates colander schemas from SQLAlchemy models.',
+    long_description=__doc__,
+    py_modules=['colander_alchemy'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['Flask'],
+    #test_suite='test_colander_alchemy.suite',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
